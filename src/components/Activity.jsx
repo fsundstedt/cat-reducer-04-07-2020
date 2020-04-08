d215a184f41c9cc56c62b442eac9fd2a39c175d2
Frank Sundstedt <frank.sundstedt@gmail.com>
2020-04-08 09:20:28 -0400

    basic exercise functioning.

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { eat } from '../redux/actions';
+import React from "react";
+import { connect } from "react-redux";
+import { setActivity } from "../redux/actions";
 
-const Activity = ({ activity, eat }) => (
+import UpdateName from "./UpdateName";
+
+const Activity = ({activity, name, setActivity}) => (
     <>
-    <h1>What is the cat doing</h1>
-    <p>The cat is: {activity}</p>
-    <button onClick={()=> eat()}>Eating</button>
+        <h1>What is the cat doing?</h1>
+        <p>{name} is {activity}</p>
+        <button onClick={() => setActivity('eating')}>Eating</button>
+        <button onClick={() => setActivity('playing')}>Playing</button>
+        <button onClick={() => setActivity('napping')}>Napping</button>
+        <UpdateName />
     </>
 )
-
 const mapStateToProps = state => {
     const { activity } = state;
     return activity;
 }
-
 export default connect(
-    mapStateToProps, // what do I want
-    { eat }  // what do I want to do
+    mapStateToProps, // mapStateToProps - What do I want?
+    { setActivity } // mapDispatchToProps - What do I want to do?
 )(Activity);
